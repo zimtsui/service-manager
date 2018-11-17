803525d91e4eb1278ccdb7c8a364517b70c8c1c6
Zim <zimtsui@gmail.com>
2018-11-17 18:42:59 +0800

    base

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import EventEmitter from 'events';
+import Promise from 'bluebird';
+
 class Service {
     constructor(manager, name) {
         Object.assign(this, {
@@ -5,72 +8,109 @@ class Service {
             name,
             __dependencies: [],
             supports: [],
+            __registry: null,
+            __reusable: false,
+            starting: null,
+            stopping: Promise.resolve(),
+            ServiceClass: null,
+            service: ::manager.service,
+            startAll: ::manager.startAll,
+            stopAll: ::manager.stopAll,
         });
     }
 
-    dependencies(dependencies) {
+    error(error) {
+        this.__manager.emit('error', error);
+    }
+
+    fatal(reusable = false) {
+        this.starting = null;
+        this.stopping = Promise.resolve();
+        if (!reusable) this.__constructService();
+        return Promise
+            .resolve(this.supports)
+            .map(support => this.__manager.services[support].stop())
+            .then(() => this.__manager.emit('fatal'));
+    }
+
+    dependencies(...dependencies) {
         if (dependencies) {
             this.__dependencies.push(...dependencies);
             dependencies.forEach((dependency) => {
-                this.__manager.service(dependency).supports.push(this.name);
+                this.service(dependency).supports.push(this.name);
             });
             return this;
-        }
-        return this.__dependencies;
+        } return this.__dependencies;
     }
 
-    object(object) {
-        this.__object = object;
-        return this;
+    reusable(reusable) {
+        if (reusable) {
+            this.__reusable = reusable;
+            return this;
+        } return this.__reusable;
+    }
+
+    registry(ServiceClass, ...args) {
+        if (ServiceClass) {
+            this.__ServiceClass = ServiceClass;
+            this.__args = args;
+            this.__constructService();
+            return this;
+        } return this.__registry;
+    }
+
+    __constructService() {
+        this.__registry = new this.__ServiceClass(this, this.__args);
     }
 
     start() {
-        if (!this.stopping) {
-            this.starting ||= Promise
-                .resolve(this.dependencies())
-                .map(dependency => this.__manager.service(dependency).start())
-                .then(::this.__object.start)
-                .then(() => {
-                    this.state = Service.States.STARTED;
-                });
-            return this.starting;
-        } return Promise.reject();
+        if (this.stopping?.isPending()) return Promise.reject();
+        this.stopping = null;
+        this.starting ||= Promise
+            .resolve(this.__dependencies)
+            .map(dependency => this.__manager.services[dependency].start())
+            .then(::this.__registry.start);
+        return this.starting;
     }
 
     stop() {
-        if (this.starting?.isFulfilled()) {
-            this.stopping ||= Promise
-                .resolve(this.supports)
-                .map(support => this.__manager.service(support).stop())
-                .then(::this.__object.stop)
-                .then(() => {
-                    this.state = Service.States.STOPPED;
-                });
-            return this.stopping;
-        } return Promise.reject();
+        if (this.starting?.isPending()) return Promise.reject();
+        this.starting = null;
+        this.stopping ||= Promise
+            .resolve(this.supports)
+            .map(support => this.__manager.services[support].stop())
+            .then(::this.__registry.stop)
+            .then(() => {
+                if (!this.__reusable) this.__constructService();
+            });
+        return this.stopping;
     }
 }
 
-class ServiceManager {
+class ServiceManager extends EventEmitter {
     constructor() {
-        this.__services = {};
+        super();
+
+        Object.assign(this, {
+            services: {},
+        });
     }
 
     service(serviceName) {
-        this.__services[serviceName] ||= new Service(this, serviceName);
-        return this.__services[serviceName];
+        this.services[serviceName] ||= new Service(this, serviceName);
+        return this.services[serviceName];
     }
 
     startAll() {
         return Promise
-            .resolve(this.__services)
+            .resolve(this.services)
             .then(::Object.values)
             .map(service => service.start());
     }
 
     stopAll() {
         return Promise
-            .resolve(this.__services)
+            .resolve(this.services)
             .then(::Object.values)
             .map(service => service.stop());
     }
