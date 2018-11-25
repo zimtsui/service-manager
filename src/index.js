6a40215159002933c2720c100ed61de2013b3fc2
Zim <zimtsui@gmail.com>
2018-11-25 21:38:58 +0800

    divided private and public members

--- src/index.js
+++ src/index.js
@@ -5,18 +5,36 @@ class ServiceAccessor {
     constructor(manager, name) {
         Object.assign(this, {
             __manager: manager,
-            name,
-            __dependencies: [],
-            supports: [],
+            __name: name,
+            // __dependencies and __supports are mutable Iterable.
+            __dependencies: new Set(),
+            __supports: new Set(),
             __registry: null,
             __reusable: false,
-            starting: null,
-            stopping: Promise.resolve(),
-            ServiceClass: null,
-            service: ::manager.service,
-            startAll: ::manager.startAll,
-            stopAll: ::manager.stopAll,
+            __starting: null,
+            __stopping: Promise.resolve(),
+            __ServiceClass: null,
         });
+    }
+
+    get name() {
+        return this.__name;
+    }
+
+    get supports() {
+        return this.__supports;
+    }
+
+    get service() {
+        return ::this.__manager.service;
+    }
+
+    get startAll() {
+        return ::this.__manager.startAll;
+    }
+
+    get stopAll() {
+        return ::this.__manager.stopAll;
     }
 
     error(error) {
@@ -24,21 +42,35 @@ class ServiceAccessor {
     }
 
     fatal(reusable = this.__reusable) {
-        this.starting = null;
-        this.stopping = Promise.resolve();
+        this.__starting = null;
+        this.__stopping = Promise.resolve();
         if (!reusable) this.__registry = null;
         return Promise
-            .resolve(this.supports)
-            .map(support => this.__manager.services[support].stop())
-            .then(() => this.__manager.emit('fatal', this.name));
+            .resolve(this.__supports)
+            .map(support => this.service(support).stop())
+            .then(() => this.__manager.emit('fatal', this.__name));
+    }
+
+    deleteDependencies(...dependencies) {
+        dependencies.forEach((dependency) => {
+            this.service(dependency).supports.delete(this.__name);
+            this.__dependencies.delete(dependency);
+        });
+        return this;
+    }
+
+    addDependencies(...dependencies) {
+        dependencies.forEach((dependency) => {
+            this.__dependencies.add(dependency);
+            this.service(dependency).supports.add(this.__name);
+        });
+        return this;
     }
 
     dependencies(...dependencies) {
-        if (dependencies) {
-            this.__dependencies.push(...dependencies);
-            dependencies.forEach((dependency) => {
-                this.service(dependency).supports.push(this.name);
-            });
+        if (dependencies.length) {
+            this.deleteDependencies(...this.__dependencies);
+            this.addDependencies(...dependencies);
             return this;
         } return this.__dependencies;
     }
@@ -57,36 +89,54 @@ class ServiceAccessor {
             this.__registry = null;
             return this;
         }
-        if (this.starting?.isFulfilled()) return this.__registry;
+        if (this.isStarted()) return this.__registry;
         throw new Error('cannot get service instance when the service is not running');
     }
 
     __constructService() {
+        if (!this.__ServiceClass) throw new Error('service class is not mounted');
         this.__registry ||= new this.__ServiceClass(this, ...this.__args);
+        return this.__registry;
     }
 
     start() {
-        if (this.stopping?.isPending()) return Promise.reject();
-        this.stopping = null;
-        this.starting ||= Promise
+        if (this.isStopping()) return Promise.reject();
+        this.__stopping = null;
+        this.__starting ||= Promise
             .resolve(this.__dependencies)
-            .map(dependency => this.__manager.services[dependency].start())
+            .map(dependency => this.service(dependency).start())
             .then(::this.__constructService)
-            .then(() => this.__registry.start());
-        return this.starting;
+            .then(registry => registry.start());
+        return this.__starting;
     }
 
     stop() {
-        if (this.starting?.isPending()) return Promise.reject();
-        this.starting = null;
-        this.stopping ||= Promise
-            .resolve(this.supports)
-            .map(support => this.__manager.services[support].stop())
+        if (this.isStarting()) return Promise.reject();
+        this.__starting = null;
+        this.__stopping ||= Promise
+            .resolve(this.__supports)
+            .map(support => this.service(support).stop())
             .then(::this.__registry.stop)
             .then(() => {
                 if (!this.__reusable) this.__registry = null;
             });
-        return this.stopping;
+        return this.__stopping;
+    }
+
+    isStarting() {
+        return this.__starting?.isPending();
+    }
+
+    isStarted() {
+        return this.__starting?.isFulfilled();
+    }
+
+    isStopping() {
+        return this.__stopping?.isPending();
+    }
+
+    isStopped() {
+        return this.__stopping?.isFulfilled();
     }
 }
 
@@ -95,25 +145,25 @@ class ServiceManager extends EventEmitter {
         super();
 
         Object.assign(this, {
-            services: {},
+            __services: {},
         });
     }
 
     service(serviceName) {
-        this.services[serviceName] ||= new ServiceAccessor(this, serviceName);
-        return this.services[serviceName];
+        this.__services[serviceName] ||= new ServiceAccessor(this, serviceName);
+        return this.__services[serviceName];
     }
 
     startAll() {
         return Promise
-            .resolve(this.services)
+            .resolve(this.__services)
             .then(::Object.values)
             .map(service => service.start());
     }
 
     stopAll() {
         return Promise
-            .resolve(this.services)
+            .resolve(this.__services)
             .then(::Object.values)
             .map(service => service.stop());
     }
